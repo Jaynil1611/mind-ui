2be69e75547eccfaa3602ffb5f04aabb75057d43
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-03-15 16:29:51 +0530

    feat: added nav component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,79 @@
 import React from "react";
 import "./Navigation.css";
+import "../List/List.css";
 
 function Navigation(props) {
-  return <div></div>;
+  return (
+    <div className="navigation-container">
+      <h3> Nvaigation </h3>
+      <p>
+        Navigation component is used as a medium to jump between different pages
+        or components of a website.
+      </p>
+      <h3> Usage </h3>
+      <h3> Desktop Navigation </h3>
+      <p> Left Nav </p>
+      <div className="navigation-examples">
+        <ul className="navbar navbar-left">
+          <li>
+            <a className="nav-link" href="nav#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="nav#">
+              Showcase
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="nav#">
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="nav#">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="nav#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      <p> Right Nav </p>
+      <div className="navigation-examples">
+        <ul className="navbar navbar-right">
+          <li>
+            <a className="nav-link" href="nav#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="nav#">
+              Showcase
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="nav#">
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="nav#">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="nav#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
